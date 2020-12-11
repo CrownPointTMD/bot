b16d2dd2df93fe7eafa75c1428c74bb09ad70312
Dr. CrownPointTMD <69466128+CrownPointTMD@users.noreply.github.com>
2020-12-11 17:23:23 +0000

    Create embed.js

---
+++ embed.js
@@ -0,0 +1,9 @@
+const EmbedTest = new Discord.MessageEmbed()
+    .setColor('#ff8c00) 
+    .setTitle('Title')
+    .setDescription('Description')
+    
+
+channel.send(EmbedTest);
+
+// Orange Embed.
