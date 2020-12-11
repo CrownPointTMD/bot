c24d85efd29de4b2b633d36247aa8f33ebb66d9d
Dr. CrownPointTMD <69466128+CrownPointTMD@users.noreply.github.com>
2020-12-11 17:24:59 +0000

    Create embed.py

---
+++ embed.py
@@ -0,0 +1,8 @@
+test = discord.Embed(
+                title = "Title",
+                description = f"Description",
+                colour = discord.Colour.orange())
+            await ctx.send(embed=test)
+           
+           
+           
